7ceeab458982b53b1c234ff6f28b622b5bd92e6a
Tariq5777 <tariq.a99@gmail.com>
2021-05-27 19:29:47 +0530

    changes

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,13 +1,18 @@
 // import { ArrowLeft, ArrowRight, SchoolRounded } from "@material-ui/icons";
-import { Divider, List, ListItem, ListItemIcon, Typography } from '@material-ui/core';
-import { Link, Redirect } from 'react-router-dom';
-import { useContext } from 'react';
+import {
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    Typography,
+} from "@material-ui/core";
+import { Link, Redirect } from "react-router-dom";
+import { useContext } from "react";
 import { UserStatusContext } from "../helper/UserStatusContext";
-import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
-import LiveHelpRoundedIcon from '@material-ui/icons/LiveHelpRounded';
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import LiveHelpRoundedIcon from "@material-ui/icons/LiveHelpRounded";
 const DropdownMenu = () => {
-
     const { user, setUser } = useContext(UserStatusContext);
 
     // const { token } = isAuthenticated();
@@ -25,27 +30,43 @@ const DropdownMenu = () => {
 
     return (
         <div className="dropdown">
-        <List className ="menu-items">
-            {["Profile", "Support", "Logout"].map(
-                (text, index) =>(
+            <List className="menu-items">
+                {["Profile", "Support", "Logout"].map((text, index) => (
                     <ListItem button key={text}>
-                        <Divider/>
-                        <ListItemIcon>
-                        {index === 0 && <AccountCircleRoundedIcon color="secondary" fontSize="large"/>}
-                        {index === 1 && <LiveHelpRoundedIcon color="secondary" fontSize="large" />}
-                        {index === 2 && <ExitToAppOutlinedIcon color="secondary" fontSize="large"/>}
-                        </ListItemIcon>
-                        {index === 0 && <Link className = "dropdown-links" to="/profile" >{text}</Link>}
-                        {index === 1 && <Link className = "dropdown-links" to="/support" >{text}</Link>}
-                        {index === 2 && <Link className = "dropdown-links" onClick={handleLogout}>{text}</Link>}
 
+                        {index === 0 && (
+                            <Link className="dropdown-links" to="/profile">
+                                <ListItemIcon>
+                                    <AccountCircleRoundedIcon fontSize="large" />
+                                </ListItemIcon>
+                                    <Typography>{text}</Typography>
+                            </Link>
+                        )}
+                      
+                        {index === 1 && (
+                            <Link className="dropdown-links" to="/support">
+                                <ListItemIcon>
+                                    <LiveHelpRoundedIcon fontSize="large" />
+                                </ListItemIcon>
+                                    <Typography>{text}</Typography>
+                            </Link>
+                        )}
+                      
+                        {index === 2 && (
+                            <Link className="dropdown-links"
+                                onClick={handleLogout}
+                            >
+                                <ListItemIcon>
+                                    <ExitToAppOutlinedIcon fontSize="large" />
+                                </ListItemIcon>
+                                    <Typography>{text}</Typography>
+                            </Link>
+                        )}
                     </ListItem>
-                )
-            )}
-        </List>
-
+                ))}
+            </List>
         </div>
     );
-}
- 
+};
+
 export default DropdownMenu;
