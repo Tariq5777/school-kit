347b63e63b62ee8d18b52d195959c2f62a316b1a
Tariq Iqbal <47449802+tariqiqbal27@users.noreply.github.com>
2021-06-01 03:52:43 +0530

    Updated Add Timetable.js

--- src/pages/Timetable/AddTimetable.js
+++ src/pages/Timetable/AddTimetable.js
@@ -1,83 +1,33 @@
-import { Container, Grid, Paper, Typography } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import {
     Button,
     Dropdown,
+    DropdownButton,
+    Table,
+    Row,
+    Col,
+    Container,
     Form,
     FormControl,
     InputGroup,
 } from "react-bootstrap";
 import Timetable from "../../components/Timetable";
 import { isAuthenticated } from "../../helper/auth/authUtils";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AddTimetable = () => {
+
     const [timetable, setTimetable] = useState({
-        standard: 0,
-        section: "",
-        timetable: {
-            0: {
-                1: "MONDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-            1: {
-                1: "TUESDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-            2: {
-                1: "WEDNESDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-            3: {
-                1: "THURSDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-            4: {
-                1: "FRIDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-            5: {
-                1: "SATURDAY",
-                2: "",
-                3: "",
-                4: "",
-                5: "",
-                6: "",
-                7: "",
-                8: "",
-            },
-        },
+        0: { 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-" },
+        1: { 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-" },
+        2: { 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-" },
+        3: { 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-" },
+        4: { 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-" },
     });
+    const [ttStandard, setTTStandard] = useState(0)
+    const [standard, setStandard] = useState([])
+    const [subjects, setSubject] = useState([])
     const schedule = [
         "8:00 - 9:00",
         "9:00 - 10:00",
@@ -88,93 +38,104 @@ const AddTimetable = () => {
         "2:00-3:00",
         "3:00-4:00",
     ];
+    const [dropdownTitle, setDropdownTitle] = useState("Select Standard")
+
+    const config = { headers: { Authorization: `Bearer ${isAuthenticated().token}` } }
+
+    const addTimetable = (e) => {
+        e.preventDefault()
+        const data = {
+            standard: standard,
+            timetable: timetable
+        }
+        //TODO: SEND POST REQUEST TO ADD TIMETABLE
+
+    }
+
+    useEffect(() => {
+        get_standard()
+        get_subject()
+    }, [])
+
+    const get_standard = () => {
+        axios.get("api/standard/", config).then(res =>
+            setStandard(res.data))
+    }
+
+    const get_subject = () => {
+        axios.get("api/subjects/", config).then(res =>
+            setSubject(res.data))
+    }
 
     const [isComplete, setIsComplete] = useState(false);
 
-    
+    useEffect(() => { console.log(timetable) }, [timetable])
+
 
     return (
         <div>
-            <Grid
-                container
-                style={{
-                    padding: "1rem",
-                    margin: "1rem",
-                    display: "flex",
-                    justifyContent: "space-evenly",
-                    alignItems: "center",
-                }}
-            >
-                <Grid item style={{ display: "flex", margin: "1rem" }}>
-                    <Dropdown
-                        style={{
-                            top: 0,
-                            overflow: "unset",
-                            background: "none",
-                            border: 0,
-                            width: "inherit",
-                            right: "0px",
-                        }}
-                    >
-                        <Dropdown.Toggle
-                            variant="secondary"
-                            id="dropdown-basic"
-                        >
-                            SELECT Standard
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, key) => (
+            <Container>
+                <Row style={{ marginTop: "10%" }}>
+                    <Col>
+                        <DropdownButton
+                            title={dropdownTitle}
+                            onSelect={(e) => setTTStandard(e)}>
+                            {standard.map(num =>
                                 <Dropdown.Item
-                                    key={key}
-                                    onChange={() =>
-                                        (timetable.standard = { num })
-                                    }
-                                >
-                                    {num}{" "}
-                                </Dropdown.Item>
-                            ))}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown
-                        style={{
-                            top: 0,
-                            overflow: "unset",
-                            background: "none",
-                            border: 0,
-                            width: "inherit",
-                            right: "0px",
-                        }}
-                    >
-                        <Dropdown.Toggle
-                            variant="secondary"
-                            id="dropdown-basic"
-                        >
-                            SELECT Section
-                        </Dropdown.Toggle>
+                                    key={num.id}
 
-                        <Dropdown.Menu>
-                            {["A", "B", "C"].map((text, key) => (
-                                <Dropdown.Item
-                                    key={key}
-                                    onChange={() =>
-                                        (timetable.section = { text })
-                                    }
+                                    eventKey={num.id}
+                                    onSelect={() => setDropdownTitle(num.standard + " " + num.section)}
                                 >
-                                    {text}
+                                    {num.standard + " " + num.section}
                                 </Dropdown.Item>
-                            ))}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Grid>
-                <Grid item>
-                    <Button variant="secondary" onClick={() => setIsComplete(true)}>Add</Button>
-                </Grid>
-            </Grid>
-            {isComplete && (
-                <Timetable timetable={timetable} schedule={schedule} />
-            )}
+                            )}
+                        </DropdownButton>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="mt-4"><p className="text-danger font-weight-bold">"- for FREE PERIOD"</p></Col></Row>
+                <Row>
+                    <Col xs={12}>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    {schedule.map((sch, index) => <th key={index}>{sch}</th>)}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.entries(timetable).map(row =>
+                                    <tr key={row[0]}>
+                                        {Object.entries(timetable[row[0]]).map((col, index) =>
+                                            <td key={index}>
+                                                {/*console.log(timetable[row[0]][col[0]])*/}
+                                                <select
+                                                    key={col[0]}
+                                                    onChange={(e) => {
+                                                        var tt = { ...timetable }
+                                                        tt[row[0]][col[0]] = e.target.value
+                                                        setTimetable({ ...tt })
+                                                    }}
+                                                    className="form-select form-select-lg mb-3">
+                                                    {subjects.map(subject =>
+                                                        <option value={subject.subject}>{subject.subject}</option>
+                                                    )}
+                                                </select>
+                                            </td>
+                                        )}
+                                    </tr>
+                                )}
+
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+                <Row className="flex-row-reverse">
+                    <Button className="mx-3" variant="success" onClick={() => setIsComplete(true)}>Create TimeTable</Button>
+                </Row>
+            </Container>
         </div>
-    );
-};
+    )
+}
 
 export default AddTimetable;
